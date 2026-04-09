5fb4792b1087f2aa8813bbfa30333764345c06cd
Bartosz Walczyk <walczykbartosz03@gmail.com>
2026-04-09 21:34:28 +0200

    added simple routing

---
+++ Routing.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once 'src/controllers/SecurityController.php';
+require_once 'src/controllers/DashboardController.php';
+
+// TODO musimy zapewnic, ze utworzony 
+// obiekt kontrollera ma tylko jedna instancję - SINGLETON
+
+// TODO 2 /dashboard -- wszystkei dnae
+// /dashboard/12234 -- wyciagnie nam jakis elemtn o wskaznaym ID 12234
+// REGEX
+class Routing {
+
+    public static $routes = [
+        "login" => [
+            "controller" => "SecurityController",
+            "action" => "login"
+        ],
+        "dashboard" => [
+            "controller" => "DashboardController",
+            "action" => "index"
+        ],
+        "" => [
+            "controller" => "SecurityController",
+            "action" => "login"
+        ],
+    ];
+
+    public static function run(string $path) {
+        if (!array_key_exists($path, Routing::$routes)) {
+            include 'public/views/404.html';
+
+            return;
+        }
+
+        switch($path) {
+            case 'dashboard':
+            case '':
+            case 'login':
+                $controller = Routing::$routes[$path]["controller"];
+                $action = Routing::$routes[$path]["action"];
+
+                $controllerObj = $controller::getInstance();
+                $id = null;
+
+                $controllerObj->$action($id);
+                break; 
+            default:
+                include 'public/views/404.html';
+                break;
+        }
+    }
+}
